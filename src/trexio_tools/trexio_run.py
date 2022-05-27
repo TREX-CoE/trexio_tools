0f7378d36ff5f82be59ed723c5f9e83f3184f405
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2022-05-27 16:20:03 +0200

    Update the entry point script + docopt

--- src/trexio_tools/trexio_run.py
+++ src/trexio_tools/trexio_run.py
@@ -3,21 +3,25 @@
 Set of tools to interact with trexio files.
 
 Usage:
-      trexio check-basis   [-n N_POINTS]  [-b BACK_END]  TREXIO_FILE
-      trexio check-mos     [-n N_POINTS]  [-b BACK_END]  TREXIO_FILE
-      trexio convert-to     -t TYPE -o OUTPUT_FILE       TREXIO_FILE
-      trexio convert-from   -t TYPE -i INPUT_FILE  [-x MO_TYPE]  [-b BACK_END]  TREXIO_FILE
-      trexio convert2champ  -i GAMESS_INPUT_FILE   [-x MO_TYPE]  [-b BACK_END]  TREXIO_FILE
+      trexio check-basis      [-n N_POINTS]  [-b BACK_END]  TREXIO_FILE
+      trexio check-mos        [-n N_POINTS]  [-b BACK_END]  TREXIO_FILE
+      trexio convert-to       -t TYPE -o OUTPUT_FILE        TREXIO_FILE
+      trexio convert-from     -t TYPE -i INPUT_FILE  [-x MO_TYPE]  [-b BACK_END]  TREXIO_FILE
+      trexio convert2champ    -i INPUT_FILE   [-x MO_TYPE]  [-b BACK_END]  TREXIO_FILE
+      trexio convert-backend  -i INPUT_FILE  -o OUTPUT_FILE  -b BACK_END  -j TREX_JSON_FILE  [-s BACK_END_FROM]  [-w OVERWRITE]
       trexio (-h | --help)
 
 Options:
-      -h --help                 Print help message.
-      -n --n_points=N_POINTS    Number of integration points. [default: 81]
-      -i --input=INPUT_FILE     Name of the input file.
-      -o --output=OUTPUT_FILE   Name of the output file.
-      -b --back_end=BACK_END    [hdf5 | text]  The TREXIO back end. [default: hdf5]
-      -t --type=TYPE            [gaussian | gamess | fcidump | molden | champ | cartesian | spherical ] File format.
-      -x --motype=MO_TYPE       [natural | initial | guga-initial | guga-natural] The type of the molecular orbitals.
+      -h, --help                    Print help message.
+      -n, --n_points=N_POINTS       Number of integration points.  [default: 81]
+      -i, --input=INPUT_FILE        Name of the input file.
+      -o, --output=OUTPUT_FILE      Name of the output file.
+      -b, --back_end=BACK_END       [hdf5 | text]  The TREXIO back end.  [default: hdf5]
+      -s, --back_end_from=BACK_END  [hdf5 | text | auto]  The input TREXIO back end.  [default: auto]
+      -j, --json=TREX_JSON_FILE     TREX configuration file (in JSON format).
+      -w, --overwrite=OVERWRITE     Overwrite flag for the conversion of back ends.  [default: True]
+      -t, --type=TYPE               [gaussian | gamess | fcidump | molden | champ | cartesian | spherical ] File format.
+      -x, --motype=MO_TYPE          [natural | initial | guga-initial | guga-natural] The type of the molecular orbitals.
 """
 
 from docopt import docopt
@@ -32,13 +36,21 @@ def main(filename=None, args=None):
         args = docopt(__doc__)
         filename = args["TREXIO_FILE"]
 
+    print(args)
 
-    if args["--n_points"] is not None:
+    if args["--n_points"]:
        n_points = int(args["--n_points"])
     else:
        n_points = 81
 
-    if args["--back_end"] is not None:
+    if args["convert-backend"]:
+        if str(args["--overwrite"]).lower() in ['false', '0']:
+            overwrite = False
+        else:
+            overwrite = True
+            print(f'File {args["--output"]} will be overwritten.')
+
+    if args["--back_end"]:
         if str(args["--back_end"]).lower() == "hdf5":
             back_end = trexio.TREXIO_HDF5
         elif str(args["--back_end"]).lower() == "text":
@@ -46,7 +58,22 @@ def main(filename=None, args=None):
         else:
             raise ValueError("Supported back ends: text, hdf5.")
     else:
-        back_end = trexio.TREXIO_HDF5
+        if args["convert-backend"]:
+            raise Exception("Missing argument for the target back end: specify --back_end or -b.")
+        else:
+            back_end = trexio.TREXIO_HDF5
+
+    if args["--back_end_from"]:
+        if str(args["--back_end_from"]).lower() == "hdf5":
+            back_end_from = trexio.TREXIO_HDF5
+        elif str(args["--back_end_from"]).lower() == "text":
+            back_end_from = trexio.TREXIO_TEXT
+        elif str(args["--back_end_from"]).lower() == "auto":
+            back_end_from = trexio.TREXIO_AUTO
+        else:
+            raise ValueError("Supported options: text, hdf5, auto (default).")
+    else:
+        back_end_from = trexio.TREXIO_AUTO
 
 
     if args["check-basis"]:
@@ -77,14 +104,22 @@ def main(filename=None, args=None):
         from converters.convert_to import run
         run(args["TREXIO_FILE"], args["--output"], args["--type"])
 
+    elif args["convert-backend"]:
+        from converters.convert_back_end import run
+        run(
+            args["--input"],
+            args["--output"],
+            back_end_to=back_end,
+            back_end_from=back_end_from,
+            overwrite=overwrite,
+            json_filename=args["--json"]
+            )
+
     else:
         pass
-
 
 
 if __name__ == '__main__':
     args = docopt(__doc__)
     filename = args["TREXIO_FILE"]
     main(filename, args)
-
-
