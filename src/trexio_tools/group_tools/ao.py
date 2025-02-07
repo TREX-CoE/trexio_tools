Fixed normalization in ao values and refactor check-basis and check-mos

src/trexio_tools/group_tools/ao.py
@@ -38,6 +38,8 @@ def value(ao,r):
     basis_num      =  basis["shell_num"]
     prim_num       =  basis["prim_num"]
     shell_ang_mom  =  basis["shell_ang_mom"]
+    shell_factor   =  basis["shell_factor"]
+    shell_index    =  [ i for i in basis["shell_index"] ]
 
     # to reconstruct for compatibility with TREXIO < v.2.0.0
     basis_old         = ao["basis_old"]
@@ -53,7 +55,7 @@ def value(ao,r):
 
     # Compute all primitives and powers
     prims = np.zeros(prim_num)
-    pows  = [ None for i in range(basis_num) ]
+    pows  = [ 0. for i in range(basis_num) ]
 
     for i_nucl in range(nucleus_num):
 
@@ -61,18 +63,16 @@ def value(ao,r):
        i_prim = shell_prim_index[i_shell]
        istart = i_prim
 
+       dr = r - coord[i_nucl]
+       r2 = dr[0]*dr[0] + dr[1]*dr[1] + dr[2]*dr[2]
+
        try:
          i_shell_end = nucleus_index[i_nucl+1]
          i_prim = shell_prim_index[i_shell_end]
          iend = i_prim
        except IndexError:
-         iend = prim_num+1
+         iend = prim_num
          i_shell_end = basis_num
-
-       dr = r - coord[i_nucl]
-       r2 = dr[0]*dr[0] + dr[1]*dr[1] + dr[2]*dr[2]
-       expo_r = exponent[istart:iend] * r2
-       prims[istart:iend] = coefficient[istart:iend] * np.exp(-expo_r)
 
        old = None
        for i in range(i_shell,i_shell_end):
@@ -81,6 +81,10 @@ def value(ao,r):
             x = np.array([ np.power(dr, p) for p in powers[old] ])
             x = np.prod(x,axis=1)
          pows[i] = x
+
+       for iprim in range(istart,iend):
+         f = shell_factor[ shell_index[iprim] ]
+         prims[iprim] = coefficient[iprim] * np.exp(-exponent[iprim]*r2) * f
 
     # Compute contractions
     rr = np.zeros(basis_num)

src/trexio_tools/group_tools/check_basis.py
@@ -6,120 +6,81 @@ from . import nucleus as trexio_nucleus
 from . import basis as trexio_basis
 from . import ao as trexio_ao
 
-try:
-    import qmckl
-    def run(trexio_file, n_points):
-        """
-        Computes numerically the overlap matrix in the AO basis and compares it to
-        the matrix stored in the file.
-        """
+def run(trexio_file, n_points):
+    """
+    Computes numerically the overlap matrix in the AO basis and compares it to
+    the matrix stored in the file.
+    """
+
+    print(trexio.read_basis_type(trexio_file))
+    if trexio.read_basis_type(trexio_file) == "Numerical":
+        from . import nao as trexio_ao
+    else:
+        from . import ao as trexio_ao
+    ao = trexio_ao.read(trexio_file)
+    basis = ao["basis"]
+    nucleus = basis["nucleus"]
+    assert basis["type"] == "Gaussian" or basis["type"] == "Numerical"
+
+    rmin = np.array( list([ np.min(nucleus["coord"][:,a]) for a in range(3) ]) )
+    rmax = np.array( list([ np.max(nucleus["coord"][:,a]) for a in range(3) ]) )
+
+    shift = np.array([8.,8.,8.])
+    linspace = [ None for i in range(3) ]
+    step = [ None for i in range(3) ]
+    for a in range(3):
+      linspace[a], step[a] = np.linspace(rmin[a]-shift[a], rmax[a]+shift[a], num=n_points, retstep=True)
+
+    print("Integration steps:", step)
+    dv = step[0]*step[1]*step[2]
+    ao_num = ao["num"]
+
+    point = []
+    for x in linspace[0]:
+      for y in linspace[1]:
+        for z in linspace[2]:
+           point += [ [x, y, z] ]
+    point = np.array(point)
+    point_num = len(point)
+
+    if trexio.has_ao_1e_int_overlap(trexio_file):
+        S_ex = trexio.read_ao_1e_int_overlap(trexio_file)
+    else:
+        S_ex = np.zeros((ao_num,ao_num))
+
+
+    try:
+        import qmckl
 
         trexio_filename = trexio_file.filename
         context = qmckl.context_create()
         qmckl.trexio_read(context, trexio_filename)
 
-        ao = trexio_ao.read(trexio_file)
-        basis = ao["basis"]
-        nucleus = basis["nucleus"]
-        assert basis["type"] == "Gaussian"
-
-        rmin = np.array( list([ np.min(nucleus["coord"][:,a]) for a in range(3) ]) )
-        rmax = np.array( list([ np.max(nucleus["coord"][:,a]) for a in range(3) ]) )
-
-        shift = np.array([8.,8.,8.])
-        linspace = [ None for i in range(3) ]
-        step = [ None for i in range(3) ]
-        for a in range(3):
-          linspace[a], step[a] = np.linspace(rmin[a]-shift[a], rmax[a]+shift[a], num=n_points, retstep=True)
-
-        print("Integration steps:", step)
-        dv = step[0]*step[1]*step[2]
-
-        point = []
-        for x in linspace[0]:
-          #print(".",end='',flush=True)
-          for y in linspace[1]:
-            for z in linspace[2]:
-               point += [ [x, y, z] ]
-        point = np.array(point)
-        point_num = len(point)
-        ao_num = ao["num"]
-
         qmckl.set_point(context, 'N', point_num, np.reshape(point, (point_num*3)))
         chi = qmckl.get_ao_basis_ao_value(context, point_num*ao_num)
         chi = np.reshape( chi, (point_num,ao_num) )
         S = chi.T @ chi * dv
-        print()
 
-        if trexio.has_ao_1e_int_overlap(trexio_file):
-            S_ex = trexio.read_ao_1e_int_overlap(trexio_file)
-        else:
-            S_ex = np.zeros((ao_num,ao_num))
+    except ImportError:
 
+        chi = []
+        for xyz in point:
+          chi += [ trexio_ao.value(ao, np.array(xyz)) ]
 
-        # This produces a lot of output for large molecules, maybe wrap up in ``if debug`` statement ?
-        for i in range(ao_num):
-          for j in range(i,ao_num):
-            print("%3d %3d %15f %15f"%(i,j,S[i,j],S_ex[i,j]))
-        S_diff = S - S_ex
-        print("Norm of the error: %f"%(np.linalg.norm(S_diff)))
+    chi = np.reshape( chi, (point_num,ao_num) )
+    S = chi.T @ chi * dv
+
+    print()
 
 
+    for i in range(ao_num):
+      for j in range(i,ao_num):
+        print("%3d %3d %15f %15f"%(i,j,S[i][j],S_ex[i,j]))
+    S_diff = S - S_ex
+    print("Norm of the error: %f"%(np.linalg.norm(S_diff)))
+
+    print("Diagonal entries:")
+    for i in range(ao_num):
+        print("%3d %15f"%(i,S[i][i]))
 
 
-except ImportError:
-
-    def run(trexio_file, n_points):
-        """
-        Computes numerically the overlap matrix in the AO basis and compares it to
-        the matrix stored in the file.
-        """
-
-        if not trexio.has_ao_1e_int_overlap(trexio_file):
-          raise Exception(
-            "One-electron overlap integrals are missing in the TREXIO file. Required for check-basis."
-            )
-
-        print(trexio.read_basis_type(trexio_file))
-        if trexio.read_basis_type(trexio_file) == "Numerical":
-            from . import nao as trexio_ao
-        ao = trexio_ao.read(trexio_file)
-        basis = ao["basis"]
-        nucleus = basis["nucleus"]
-        assert basis["type"] == "Gaussian" or basis["type"] == "Numerical"
-
-        rmin = np.array( list([ np.min(nucleus["coord"][:,a]) for a in range(3) ]) )
-        rmax = np.array( list([ np.max(nucleus["coord"][:,a]) for a in range(3) ]) )
-
-        shift = np.array([8.,8.,8.])
-        linspace = [ None for i in range(3) ]
-        step = [ None for i in range(3) ]
-        for a in range(3):
-          linspace[a], step[a] = np.linspace(rmin[a]-shift[a], rmax[a]+shift[a], num=n_points, retstep=True)
-
-        print("Integration steps:", step)
-        dv = step[0]*step[1]*step[2]
-        ao_num = ao["num"]
-
-        S = np.zeros( [ ao_num, ao_num ] )
-
-        for x in linspace[0]:
-          #print(".",end='',flush=True)
-          for y in linspace[1]:
-            for z in linspace[2]:
-               chi = trexio_ao.value(ao, np.array( [x,y,z] ) )
-               S += np.outer(chi, chi)*dv
-        print()
-
-        S_ex = trexio.read_ao_1e_int_overlap(trexio_file)
-
-        # This produces a lot of output for large molecules, maybe wrap up in ``if debug`` statement ?
-        for i in range(ao_num):
-          for j in range(i,ao_num):
-            print("%3d %3d %15f %15f"%(i,j,S[i][j],S_ex[i,j]))
-        S_diff = S - S_ex
-        print("Norm of the error: %f"%(np.linalg.norm(S_diff)))
-
-        print("Diagonal entries:")
-        for i in range(ao_num):
-            print("%3d %15f"%(i,S[i][i]))

src/trexio_tools/group_tools/check_mos.py
@@ -7,105 +7,71 @@ from . import basis as trexio_basis
 from . import ao as trexio_ao
 from . import mo as trexio_mo
 
-try:
-    import qmckl
-    def run(trexio_file, n_points):
-        """
-        Computes numerically the overlap matrix in the AO basis and compares it to
-        the matrix stored in the file.
-        """
+def run(trexio_file, n_points):
 
-        trexio_filename = trexio_file.filename
+    """
+    Computes numerically the overlap matrix in the AO basis and compares it to
+    the matrix stored in the file.
+    """
+
+    mo = trexio_mo.read(trexio_file)
+    ao = mo["ao"]
+    basis = ao["basis"]
+    nucleus = basis["nucleus"]
+    assert basis["type"] == "Gaussian"
+
+    rmin = np.array( list([ np.min(nucleus["coord"][:,a]) for a in range(3) ]) )
+    rmax = np.array( list([ np.max(nucleus["coord"][:,a]) for a in range(3) ]) )
+
+    shift = np.array([8.,8.,8.])
+    linspace = [ None for i in range(3) ]
+    step = [ None for i in range(3) ]
+    for a in range(3):
+      linspace[a], step[a] = np.linspace(rmin[a]-shift[a], rmax[a]+shift[a],
+                                         num=n_points, retstep=True)
+
+    print("Integration steps:", step)
+    dv = step[0]*step[1]*step[2]
+    mo_num = mo["num"]
+
+    point = []
+    for x in linspace[0]:
+      #print(".",end='',flush=True)
+      for y in linspace[1]:
+        for z in linspace[2]:
+           point += [ [x, y, z] ]
+    point = np.array(point)
+    point_num = len(point)
+
+
+    try:
+        import qmckl
+
         context = qmckl.context_create()
+        trexio_filename = trexio_file.filename
         qmckl.trexio_read(context, trexio_filename)
-
-        mo = trexio_mo.read(trexio_file)
-        ao = mo["ao"]
-        basis = ao["basis"]
-        nucleus = basis["nucleus"]
-        assert basis["type"] == "Gaussian"
-
-        rmin = np.array( list([ np.min(nucleus["coord"][:,a]) for a in range(3) ]) )
-        rmax = np.array( list([ np.max(nucleus["coord"][:,a]) for a in range(3) ]) )
-
-        shift = np.array([8.,8.,8.])
-        linspace = [ None for i in range(3) ]
-        step = [ None for i in range(3) ]
-        for a in range(3):
-          linspace[a], step[a] = np.linspace(rmin[a]-shift[a], rmax[a]+shift[a], num=n_points, retstep=True)
-
-        print("Integration steps:", step)
-        dv = step[0]*step[1]*step[2]
-
-        point = []
-        for x in linspace[0]:
-          #print(".",end='',flush=True)
-          for y in linspace[1]:
-            for z in linspace[2]:
-               point += [ [x, y, z] ]
-        point = np.array(point)
-        point_num = len(point)
-        mo_num = mo["num"]
 
         qmckl.set_point(context, 'N', point_num, np.reshape(point, (point_num*3)))
         chi = qmckl.get_mo_basis_mo_value(context, point_num*mo_num)
-        chi = np.reshape( chi, (point_num,mo_num) )
-        S = chi.T @ chi * dv
-        print()
 
-        S_ex = np.eye(mo_num)
+    except ImportError:
 
-        for i in range(mo_num):
-          for j in range(i,mo_num):
-            print("%3d %3d %15f %15f"%(i,j,S[i][j],S_ex[i,j]))
-        S_diff = S - S_ex
-        print ("Norm of the error: %f"%(np.linalg.norm(S_diff)))
+        chi = []
+        for xyz in point:
+          chi += [ trexio_mo.value(mo, np.array(xyz)) ]
 
-except ImportError:
+    chi = np.reshape( chi, (point_num,mo_num) )
+    S = chi.T @ chi * dv
 
-    def run(trexio_file, n_points):
-        """
-        Computes numerically the overlap matrix in the AO basis and compares it to
-        the matrix stored in the file.
-        """
+    print()
 
-        mo = trexio_mo.read(trexio_file)
-        ao = mo["ao"]
-        basis = ao["basis"]
-        nucleus = basis["nucleus"]
-        assert basis["type"] == "Gaussian"
+    S_ex = np.eye(mo_num)
+    S_diff = S - S_ex
 
-        rmin = np.array( list([ np.min(nucleus["coord"][:,a]) for a in range(3) ]) )
-        rmax = np.array( list([ np.max(nucleus["coord"][:,a]) for a in range(3) ]) )
+    for i in range(mo_num):
+      for j in range(i,mo_num):
+        print("%3d %3d %15f %15f"%(i,j,S[i][j],S_ex[i,j]))
 
-        shift = np.array([8.,8.,8.])
-        linspace = [ None for i in range(3) ]
-        step = [ None for i in range(3) ]
-        for a in range(3):
-          linspace[a], step[a] = np.linspace(rmin[a]-shift[a], rmax[a]+shift[a], num=n_points, retstep=True)
-
-        print("Integration steps:", step)
-        dv = step[0]*step[1]*step[2]
-
-        mo_num = mo["num"]
-        S = np.zeros( [ mo_num, mo_num ] )
-        for x in linspace[0]:
-          #print(".",end='',flush=True)
-          for y in linspace[1]:
-            for z in linspace[2]:
-               chi = trexio_mo.value(mo, np.array( [x,y,z] ) )
-               S += np.outer(chi, chi)*dv
-        print()
-
-        S_ex = np.eye(mo_num)
-        S_diff = S - S_ex
-        print ("%e %e"%(np.linalg.norm(S), np.linalg.norm(S_ex) ))
-        print ("Norm of the error: %e"%(np.linalg.norm(S_diff)))
-        #print(S_diff)
-
-        for i in range(mo_num):
-          for j in range(i,mo_num):
-            print("%3d %3d %15f %15f"%(i,j,S[i][j],S_ex[i,j]))
-
-
+    print ("Norm of the error: %e"%(np.linalg.norm(S_diff)))
+    #print(S_diff)
 